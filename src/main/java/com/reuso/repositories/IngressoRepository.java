bcee41c309ad911dff5f17a7283276f34eefb501
TiagoPaulin <thiago.vmp15@gmail.com>
2024-10-13 12:28:38 -0300

    visualizar_ingressso integracao

--- src/main/java/com/reuso/repositories/IngressoRepository.java
+++ src/main/java/com/reuso/repositories/IngressoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.reuso.entities.Ingresso;
 
 public interface IngressoRepository extends JpaRepository<Ingresso, Long>{
-	
+
 }
